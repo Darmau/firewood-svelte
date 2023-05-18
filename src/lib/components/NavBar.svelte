6c06282ac1f2c8947d0e80dd37fe916839249da8
李大毛 <dev@darmau.design>
2023-05-18 23:47:20 +0800

    feat: add navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,94 @@
+<script>
+	let menuShown = false;
+	const toogleMenu = () => {
+		menuShown = !menuShown;
+	};
+
+	const navItems = [
+		{ label: '首页', href: '/' },
+		{ label: '推荐', href: '/' },
+		{ label: '博客', href: '/' },
+		{ label: '关于', href: '/' }
+	];
+</script>
+
+<header class="bg-white">
+	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+		<a href="/" class="-m-1.5 p-1.5">
+			<img
+				class="h-8 w-auto"
+				src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+				alt=""
+			/>
+		</a>
+		<div class="flex lg:hidden">
+			<button
+				type="button"
+				class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 cursor-pointer"
+				on:click={toogleMenu}
+			>
+				<svg
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div class="hidden lg:flex lg:gap-x-12">
+			{#each navItems as nav }
+        <a href={nav.href} class="text-sm font-semibold leading-6 text-gray-900">{nav.label}</a>
+      {/each}
+		</div>
+	</nav>
+
+	<!-- Mobile menu, show/hide based on menu open state. -->
+	<div class={`${menuShown ? 'block' : 'hidden'} lg:hidden`} role="dialog" aria-modal="true">
+		<!-- Background backdrop, show/hide based on slide-over state. -->
+		<div class="fixed inset-0 z-10 bg-gray-500/30" on:click={toogleMenu} on:keydown={toogleMenu} />
+		<div
+			class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+		>
+			<div class="flex items-center justify-between">
+				<a href="/" class="-m-1.5 p-1.5">
+					<span class="sr-only">Your Company</span>
+					<img
+						class="h-8 w-auto"
+						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+						alt=""
+					/>
+				</a>
+				<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" on:click={toogleMenu}>
+					<span class="sr-only">Close menu</span>
+					<svg
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+			<div class="mt-6 flow-root">
+				<div class="-my-6 divide-y divide-gray-500/10">
+					<div class="space-y-2 py-6">
+						{#each navItems as nav}
+              <a href={nav.href} class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{nav.label}</a>
+            {/each}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
