28fa47673155e9f774aa85ac00c994ce1bbdb2a4
李大毛 <dev@darmau.design>
2023-05-21 17:44:03 +0800

    微小更新

---
+++ src/lib/functions/addPageView.ts
@@ -0,0 +1,16 @@
+export default async function addPageView(
+  id: string
+): Promise<boolean> {
+  const response = await fetch(`/api/pageview?id=${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+
+  if (response.ok) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import LatestArticle from '$lib/components/LatestArticle.svelte';
-	import TopicArticle from '$lib/components/TopicArticle.svelte';
+	import LatestArticle from '$lib/components/LatestArticleSection.svelte';
+	import TopicArticle from '$lib/components/TopicArticleSection.svelte';
 	import Topics from '$lib/components/Topics.svelte';
 	import type { PageServerData } from './$types';
 	export let data: PageServerData;
 </script>
 
 <Topics />
-<main class="mx-auto max-w-7xl p-4">
-	<TopicArticle title="推荐" articles={data.feature} />
-	<LatestArticle articles={data.latest} />
-	<TopicArticle title="技术" articles={data.tech} />
-	<TopicArticle title="社会" articles={data.society} />
-	<TopicArticle title="文化" articles={data.culture} />
-	<TopicArticle title="旅行" articles={data.travel} />
-	<TopicArticle title="生活情感" articles={data.emotion} />
+<main class="mx-auto max-w-7xl px-6 mb-8 md:mb-16">
+	<TopicArticle title="推荐" articles={data.feature} link="/feature/1" />
+	<LatestArticle articles={data.latest} link="/latest/1" />
+	<TopicArticle title="技术" articles={data.tech} link="/topic/tech/1" />
+	<TopicArticle title="社会" articles={data.society} link="/topic/society/1" />
+	<TopicArticle title="文化" articles={data.culture} link="/topic/culture/1" />
+	<TopicArticle title="旅行" articles={data.travel} link="/topic/travel/1" />
+	<TopicArticle title="生活情感" articles={data.emotion} link="/topic/emotion/1" />
 </main>
