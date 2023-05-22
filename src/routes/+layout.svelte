81664c895d236df78a2e83c7c0364d1330ecbdd9
李大毛 <dev@darmau.design>
2023-05-22 16:10:53 +0800

    add umami

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,13 +18,18 @@
 
 		return () => {
 			window.removeEventListener('scroll', handleScroll);
-		}
-	})
+		};
+	});
 </script>
 
 <svelte:head>
 	<title>积薪 - 独立博客导航站</title>
 	<meta name="description" content="依然相信文字的力量" />
+	<script
+		async
+		src="https://analytics.darmau.design/script.js"
+		data-website-id="e8bdffeb-b9da-4541-afc3-4d0a8dc5664b"
+	></script>
 </svelte:head>
 
 <NavBar show={showNavBar} />
