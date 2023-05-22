9e7eaa7844299803aa153bf5e736c82f690588e8
李大毛 <dev@darmau.design>
2023-05-22 23:39:39 +0800

    feat: 增加分类浏览

---
+++ src/lib/components/TopicSection.svelte
@@ -0,0 +1,35 @@
+<script>
+	const COS_URL = 'https://firewood-1256887306.cos.accelerate.myqcloud.com/normal/';
+	const topics = [
+		{ label: '技术', href: '/topic/tech/1', cover: 'tech' },
+		{ label: '社会', href: '/topic/society/1', cover: 'society' },
+		{ label: '文化', href: '/topic/culture/1', cover: 'culture' },
+		{ label: '旅行', href: '/topic/travel/1', cover: 'travel' },
+		{ label: '生活情感', href: '/topic/emotion/1', cover: 'emotion' },
+		{ label: '历史', href: '/topic/history/1', cover: 'history' },
+		{ label: '游戏', href: '/topic/game/1', cover: 'game' },
+		{ label: '时事', href: '/topic/politics/1', cover: 'politics' },
+		{ label: '国际', href: '/topic/international/1', cover: 'international' },
+		{ label: '体育', href: '/topic/sports/1', cover: 'sports' },
+		{ label: '综合', href: '/topic/others/1', cover: 'others' },
+		{ label: '财经', href: '/topic/finance/1', cover: 'finance' }
+	];
+</script>
+
+<section class="py-8 border-b border-zinc-200 dark:border-zinc-700/40">
+	<h2 class="text-2xl font-medium text-zinc-800 dark:text-zinc-100">分类</h2>
+	<div class="grid gap-4 grid-cols-2 mt-8 md:grid-cols-3 lg:grid-cols-4">
+		{#each topics as topic}
+			<a href={topic.href}>
+				<div class="bg-gray-200 relative overflow-hidden rounded-md">
+          <p class="text-white font-medium text-lg absolute z-10 flex items-center justify-center">{topic.label}</p>
+					<picture class="w-full h-full brightness-60">
+						<source srcset={`${COS_URL}${topic.cover}.avif`} type="image/avif" />
+						<source srcset={`${COS_URL}${topic.cover}.webp`} type="image/webp" />
+						<img class="aspect-[5/2] object-cover rounded-md hover:blur hover:brightness-80 transition-all duration-300" src={`${COS_URL}${topic.cover}.jpg`} alt={topic.label} />
+					</picture>
+				</div>
+			</a>
+		{/each}
+	</div>
+</section>
