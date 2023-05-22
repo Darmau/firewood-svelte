77b4fd9e9ad5ab8cb7c64b932cdf413afff342a6
李大毛 <dev@darmau.design>
2023-05-22 09:51:40 +0800

    fix: remove flobite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,11 @@
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
 	],
-	plugins: [require('flowbite/plugin')],
+	plugins: [)],
 	theme: {
 		extend: {
-			colors: {
-				primary: {
-					50: '#ebf5ff',
-					100: '#fff1ee',
-					200: '#ffe4de',
-					300: '#ffd5cc',
-					400: '#ffbcad',
-					500: '#fe795d',
-					600: '#ef562f',
-					700: '#eb4f27',
-					800: '#d3330a',
-					900: '#d3330a'
-				}
-			}
+			
 		}
 	},
 	plugins: []
