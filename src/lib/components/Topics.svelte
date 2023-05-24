7a8416d123915e8693c536ca8cfa93e500c9db1d
李大毛 <dev@darmau.design>
2023-05-24 10:25:41 +0800

    style: improve topic style

--- src/lib/components/Topics.svelte
+++ src/lib/components/Topics.svelte
@@ -6,18 +6,20 @@
 		{ label: '旅游', href: '/topic/travel/1' },
 		{ label: '生活', href: '/topic/emotion/1' },
 		{ label: '时事', href: '/topic/politics/1' },
-		{ label: '游戏', href: '/topic/game/1' },
+		{ label: '游戏', href: '/topic/game/1' }
 	];
 </script>
 
 <div
-	class="flex justify-center p-4 w-full border-b dark:border-zinc-700 gap-x-8 text-sm leading-6 sm:w-auto sm:pl-6 sm:leading-7"
+	class="flex justify-center w-full overflow-x-scroll p-4 border-b dark:border-zinc-700 text-sm leading-6 sm:pl-6 sm:leading-7"
 >
-	{#each topicList as topic}
-		<a
-			href={topic.href}
-			class="text-zinc-800 dark:text-zinc-100 transition-all duration-150 hover:text-teal-600 hover:font-medium dark:hover:text-teal-400"
-			>{topic.label}</a
-		>
-	{/each}
+	<div class="flex items-center gap-6 md:gap-8">
+		{#each topicList as topic}
+			<a
+				href={topic.href}
+				class="inline-block whitespace-nowrap text-zinc-800 dark:text-zinc-100 transition-all duration-150 hover:text-teal-600 hover:font-medium dark:hover:text-teal-400"
+				>{topic.label}</a
+			>
+		{/each}
+	</div>
 </div>
