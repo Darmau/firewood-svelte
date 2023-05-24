7b0bf1807b0007739fd8e92fb4e8836eebc2f07e
李大毛 <dev@darmau.design>
2023-05-24 17:55:14 +0800

    style: 补充情况
    没有抓取的时候不显示最近发布板块

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -12,7 +12,9 @@
 		{#if website.cover}
 			<img src={website.cover} alt={website.name} class="w-5 h-5 dark:bg-white" />
 		{/if}
-		<h2 class="text-zinc-800 dark:text-zinc-100 text-lg font-medium hover:text-teal-600 dark:hover:text-teal-400">
+		<h2
+			class="text-zinc-800 dark:text-zinc-100 text-lg font-medium hover:text-teal-600 dark:hover:text-teal-400"
+		>
 			{website.name}
 		</h2>
 	</a>
@@ -31,16 +33,18 @@
 			<p class="text-zinc-800 dark:text-zinc-100">{generateDate(website.last_publish)}</p>
 		</div>
 	</div>
-	<div class="space-y-3">
-		<h3 class="text-sm text-zinc-400 dark:text-zinc-500">最新文章</h3>
-		<ol class="space-y-3">
-			{#each website.latest as article}
-				<li class="text-zinc-800 hover:text-teal-600 dark:text-zinc-100 dark:hover:text-teal-400">
-					<a href={article.url} target="_blank">
-						{article.title}
-					</a>
-				</li>
-			{/each}
-		</ol>
-	</div>
+	{#if website.latest}
+		<div class="space-y-3">
+			<h3 class="text-sm text-zinc-400 dark:text-zinc-500">最新文章</h3>
+			<ol class="space-y-3">
+				{#each website.latest as article}
+					<li class="text-zinc-800 hover:text-teal-600 dark:text-zinc-100 dark:hover:text-teal-400">
+						<a href={article.url} target="_blank">
+							{article.title}
+						</a>
+					</li>
+				{/each}
+			</ol>
+		</div>
+	{/if}
 </div>
