fdf2e3ba818799753ed7c80a7e4b36e096e826c2
李大毛 <dev@darmau.design>
2023-05-25 23:41:22 +0800

    add CDN support

--- src/lib/functions/getImgUrl.ts
+++ src/lib/functions/getImgUrl.ts
@@ -1,6 +1,11 @@
 export default function getImgUrl(url: string): string {
-  const regex = /(.+cos\.)myqcloud(\..+)/;
-  const replacement = '$1accelerate$2';
-  const newUrl = url.replace(regex, replacement);
-  return 'https://' + newUrl;
+  const isProduction = process.env.NODE_ENV === 'production';
+
+  if (isProduction) {
+    const cdnDomain = "https://firewood.darmau.design/";
+    const pathName = url.split('/').slice(1).join('/');
+    return cdnDomain + pathName;
+  } else {
+    return "https://" + url;
+  }
 }
