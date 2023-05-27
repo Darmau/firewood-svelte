90f9cfc5cda5e03d58b2e779677620c0a4b94f97
李大毛 <dev@darmau.design>
2023-05-28 00:13:28 +0800

    fix: 修复添加网站后还没抓取文章的样式

--- src/routes/author/latest/[page]/+page.svelte
+++ src/routes/author/latest/[page]/+page.svelte
@@ -8,17 +8,23 @@
 <svelte:head>
 	<title>作者 - 积薪</title>
 	<meta name="description" content="本站收录的所有博客" />
-	<meta property="og:url" content="https://firewood.news/author/latest/1">
-  <meta property="og:type" content="website">
-  <meta property="og:title" content="作者 - 积薪">
-  <meta property="og:description" content="本站收录的所有博客">
-  <meta property="og:image" content="https://imagedelivery.net/VyUbUTLtvl82TWWarMVgPw/4763b0f6-c1f8-4738-3a8c-e57b7fcd7000/width=1200">
-	<meta name="twitter:card" content="summary_large_image">
-  <meta property="twitter:domain" content="firewood.news">
-  <meta property="twitter:url" content="https://firewood.news/latest/1">
-  <meta name="twitter:title" content="作者 - 积薪">
-  <meta name="twitter:description" content="本站收录的所有博客">
-  <meta name="twitter:image" content="https://imagedelivery.net/VyUbUTLtvl82TWWarMVgPw/4763b0f6-c1f8-4738-3a8c-e57b7fcd7000/width=1200">
+	<meta property="og:url" content="https://firewood.news/author/latest/1" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="作者 - 积薪" />
+	<meta property="og:description" content="本站收录的所有博客" />
+	<meta
+		property="og:image"
+		content="https://imagedelivery.net/VyUbUTLtvl82TWWarMVgPw/4763b0f6-c1f8-4738-3a8c-e57b7fcd7000/width=1200"
+	/>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="firewood.news" />
+	<meta property="twitter:url" content="https://firewood.news/latest/1" />
+	<meta name="twitter:title" content="作者 - 积薪" />
+	<meta name="twitter:description" content="本站收录的所有博客" />
+	<meta
+		name="twitter:image"
+		content="https://imagedelivery.net/VyUbUTLtvl82TWWarMVgPw/4763b0f6-c1f8-4738-3a8c-e57b7fcd7000/width=1200"
+	/>
 </svelte:head>
 
 <main class="mx-auto max-w-7xl px-6 py-8 md:py-12">
@@ -27,7 +33,9 @@
 	</div>
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 		{#each data.websites as website}
-			<BlogCard {website} />
+			{#if website.article_count > 0}
+				<BlogCard {website} />
+			{/if}
 		{/each}
 	</div>
 	<Pagination
