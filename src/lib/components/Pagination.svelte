1c4a6ffc6028fb9d6f51111d4225ca1aae9e60f6
李大毛 <dev@darmau.design>
2023-05-28 20:02:02 +0800

    style: 分页器修改
    只保留上一页下一页

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -34,7 +34,7 @@
 
 {#if totalPages > 1}
 	<div class="flex items-center justify-between border-t dark:border-zinc-800 mt-8 py-8">
-		<div class="flex flex-1 justify-between sm:hidden">
+		<div class="flex flex-1 justify-between">
 			<div class={currentPage === 1 ? 'invisible pointer-events-none' : ''}>
 				<a
 					class="relative inline-flex items-center rounded-md border dark:border-zinc-700 bg-white dark:bg-zinc-800 px-4 py-2 text-sm font-medium text-slate-700 dark:text-slate-300 hover:bg-gray-50 dark:hover:bg-zinc-900"
@@ -50,84 +50,6 @@
 				>
 					下一页
 				</a>
-			</div>
-		</div>
-
-		<div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-			<div>
-				<p class="text-sm text-slate-700 dark:text-slate-300">
-					当前为 <span class="font-medium">{(currentPage - 1) * itemPerPage + 1}</span> 至
-					<span class="font-medium">{Math.min(currentPage * itemPerPage, totalArticles)}</span>
-					条，共计
-					<span class="font-medium">{totalArticles}</span>条
-				</p>
-			</div>
-
-			<div>
-				<nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-					<a
-						href={`${path}/${+currentPage - 1}`}
-						class={`relative inline-flex items-center rounded-l-md border dark:border-zinc-600 bg-white dark:bg-teal-800/10 px-2 py-2 text-sm font-medium dark:hover:bg-zinc-700 ${
-							currentPage === 1
-								? `text-slate-300 pointer-events-none	`
-								: `text-slate-500 hover:bg-gray-50 focus:z-20 dark:hover:text-slate-300`
-						}`}
-					>
-						<svg
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-4 h-4"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M15.75 19.5L8.25 12l7.5-7.5"
-							/>
-						</svg>
-					</a>
-
-					{#each pagesToRender as pageNumber}
-						{#if typeof pageNumber === 'number'}
-							<a
-								href={`${path}/${pageNumber}`}
-								class={`relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 dark:hover:bg-teal-700 ${
-									pageNumber === currentPage
-										? 'z-10 border-teal-500 bg-teal-50 text-teal-800 dark:bg-teal-900 dark:text-zinc-100'
-										: 'dark:border-gray-600 bg-white dark:bg-teal-800/10 text-zinc-500 dark:text-slate-300 hover:bg-gray-50 dark:hover:text-slate-300'
-								} ${typeof pageNumber === 'string' ? 'disabled' : ''}`}
-							>
-								{pageNumber}
-							</a>
-						{:else}
-							<div
-								class="relative inline-flex items-center border dark:border-zinc-600 bg-white dark:bg-zinc-800 px-4 py-2 text-sm font-medium text-gray-500"
-							>
-								...
-							</div>
-						{/if}
-					{/each}
-
-					<a
-						href={`${path}/${+currentPage + 1}`}
-						class={`relative inline-flex items-center rounded-r-md border dark:border-zinc-600 bg-white dark:bg-teal-800/10 px-2 py-2 text-sm font-medium dark:hover:bg-zinc-700 ${
-							currentPage === totalPages
-								? `text-slate-300 pointer-events-none`
-								: `text-slate-500 hover:bg-gray-50 focus:z-20 dark:hover:text-slate-300`
-						}`}
-					>
-						<svg
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-4 h-4"
-						>
-							<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-						</svg>
-					</a>
-				</nav>
 			</div>
 		</div>
 	</div>
