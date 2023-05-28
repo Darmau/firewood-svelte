42c0f29e887fc70c20dbcfbcdee487b0708bee01
李大毛 <dev@darmau.design>
2023-05-28 19:38:49 +0800

    feat: login page

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,48 @@
+import { loginUser, verifyUser } from '$lib/functions/user.module';
+import type { PageServerLoad } from '../$types';
+import type { Actions } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async ({cookies}) => {
+  const access_token = cookies.get('access_token');
+
+  if(access_token) {
+    const response = await verifyUser(access_token);
+    if(response) {
+      throw redirect(302, '/admin');
+    }
+  }
+}
+
+export const actions: Actions = {
+  default: async (event) => {
+    const formData = Object.fromEntries(await event.request.formData());
+
+    if (!formData.username || !formData.password) {
+      return fail(400, {
+        error: '请输入用户名和密码'
+      });
+    }
+
+    const { username, password } = formData as { username: string, password: string };
+
+    const response = await loginUser(username, password);
+
+    if(!response) {
+      return fail(401, {
+        error: '用户名或密码错误'
+      });
+    }
+
+    event.cookies.set('access_token', `${response}`, {
+      httpOnly: true,
+      path: '/',
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 14
+    })
+
+    throw redirect(302, '/admin');
+
+  }
+} satisfies Actions;
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { ActionData } from "./$types";
+  import { enhance } from "$app/forms";
+
+  export let form: ActionData;
+</script>
+
+<svelte:head>
+  <title>登录管理后台</title>
+</svelte:head>
+
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">登录管理后台</h2>
+  </div>
+
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" method="POST" use:enhance>
+      <div>
+        <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
+        <div class="mt-2">
+          <input id="username" name="username" type="text" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600">Sign in</button>
+      </div>
+    </form>
+  </div>
+</div>
