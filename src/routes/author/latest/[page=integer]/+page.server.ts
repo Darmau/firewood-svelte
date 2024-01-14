b9f7d274797e97d7d3d09887162328f547903ed8
李大毛 <dev@darmau.design>
2024-01-14 23:00:36 +0800

    perf: 移除KV逻辑

--- src/routes/author/latest/[page=integer]/+page.server.ts
+++ src/routes/author/latest/[page=integer]/+page.server.ts
@@ -1,37 +1,28 @@
-import type { PageServerLoad } from './$types';
-import {API_URL, CACHE_URL} from '$env/static/private';
-import { redirect } from '@sveltejs/kit';
+import type {PageServerLoad} from './$types';
+import {API_URL} from '$env/static/private';
+import {redirect} from '@sveltejs/kit';
 
 // 获取博客
-export const load = (async ({ params: { page }, setHeaders }) => {
-	setHeaders({
-		'Cache-Control': 'max-age=600'
-	});
-	const websites = await fetch(`${CACHE_URL}`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			key: `/website/latest?page=${page}&limit=21`
-		})
-	})
-	.then((res) => res.json());
+export const load = (async ({params: {page}, setHeaders}) => {
+  setHeaders({
+    'Cache-Control': 'max-age=600'
+  });
+  const websites = await fetch(`${API_URL}/website/latest?page=${page}&limit=21`).then((res) => res.json());
 
-	// 如果没有网站数据，重定向至第一页
-	if (websites.length === undefined || websites.length === 0) {
-		throw redirect(308, `/author/latest/1`);
-	}
+  // 如果没有网站数据，重定向至第一页
+  if (websites.length === undefined || websites.length === 0) {
+    throw redirect(308, `/author/latest/1`);
+  }
 
-	const count = await fetch(`${API_URL}/website/count`, {
-		method: 'Get',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-	return {
-		websites: websites,
-		count: count.json(),
-		page: Number(page)
-	};
+  const count = await fetch(`${API_URL}/website/count`, {
+    method: 'Get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return {
+    websites: websites,
+    count: count.json(),
+    page: Number(page)
+  };
 }) satisfies PageServerLoad;
