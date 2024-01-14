92f127416af0fbd93f1de97670a108b2fb5c99e5
李大毛 <dev@darmau.design>
2024-01-14 23:05:09 +0800

    perf: 移除KV逻辑 (#20)

--- src/routes/author/most-view/[page=integer]/+page.server.ts
+++ src/routes/author/most-view/[page=integer]/+page.server.ts
@@ -1,37 +1,28 @@
-import {API_URL, CACHE_URL} from '$env/static/private';
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
+import {API_URL} from '$env/static/private';
+import type {PageServerLoad} from './$types';
+import {redirect} from '@sveltejs/kit';
 
 // 获取博客
-export const load = (async ({ params: { page }, setHeaders }) => {
-	setHeaders({
-		'Cache-Control': 'max-age=600'
-	});
-	const websites = await fetch(`${CACHE_URL}`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			key: `/website/most-view?page=${page}&limit=21`
-		})
-	})
-	.then((res) => res.json());
+export const load = (async ({params: {page}, setHeaders}) => {
+  setHeaders({
+    'Cache-Control': 'max-age=600'
+  });
+  const websites = await fetch(`${API_URL}/website/most-view?page=${page}&limit=21`).then((res) => res.json());
 
-	// 如果没有网站数据，重定向至第一页
-	if (websites.length === undefined || websites.length === 0) {
-		throw redirect(308, `/author/latest/1`);
-	}
+  // 如果没有网站数据，重定向至第一页
+  if (websites.length === undefined || websites.length === 0) {
+    throw redirect(308, `/author/latest/1`);
+  }
 
-	const count = await fetch(`${API_URL}/website/count`, {
-		method: 'Get',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
-	return {
-		websites: websites,
-		count: count.json(),
-		page: Number(page)
-	};
+  const count = await fetch(`${API_URL}/website/count`, {
+    method: 'Get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return {
+    websites: websites,
+    count: count.json(),
+    page: Number(page)
+  };
 }) satisfies PageServerLoad;
--- src/routes/feature/[page=integer]/+page.server.ts
+++ src/routes/feature/[page=integer]/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-import { API_URL, CACHE_URL } from '$env/static/private';
+import { API_URL } from '$env/static/private';
 
 // export const prerender = true;
 
@@ -8,27 +8,9 @@ export const load = (async ({ params: { page }, setHeaders }) => {
 	setHeaders({
 		'Cache-Control': 'max-age=600'
 	});
-	const featuredArticles = await fetch(`${CACHE_URL}`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			key: `/article/featured?page=${page}&limit=15`
-		})
-	})
-	.then((res) => res.json());
+	const featuredArticles = await fetch(`${API_URL}/article/featured?page=${page}&limit=15`).then((res) => res.json());
 
-	const hottestArticles = await fetch(`${CACHE_URL}`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			key: '/article/hottest?limit=10'
-		})
-	})
-	.then((res) => res.json());
+	const hottestArticles = await fetch(`${API_URL}/article/hottest?limit=10`).then((res) => res.json());
 
 	const count = await fetch(`${API_URL}/article/count`, {
 		method: 'POST',
